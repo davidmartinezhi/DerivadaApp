bbeaa449203399c2749bad0ef049d213461c739c
David Gerardo Martinez <gerardomartinez.hi@gmail.com>
2023-08-25 19:18:42 -0600

    Initial Commit

---
+++ DerivadaApp/DerivadaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DerivadaAppApp.swift
+//  DerivadaApp
+//
+//  Created by David Gerardo Martínez on 25/08/23.
+//
+
+import SwiftUI
+
+@main
+struct DerivadaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
